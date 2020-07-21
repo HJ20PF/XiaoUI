59f2f50214f1826dac1cb24d4317a5c5b996ba2f
HJ20PF <2440910770@qq.com>
2020-07-21 18:05:43 +0800

    测试页

---
+++ pages/test/test.js
@@ -0,0 +1,74 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    banner:{
+      list:[{
+        id:17,
+        type:2,
+        title:'工长列表',
+        pic:'images/1028/2019/08/wYPdr180ZXpY8x68Yq66dqQ00xP63D.png',
+        clickago_icon:null,
+        clickafter_icon:null,
+        link_type:1,
+        url_type:null,
+        url:'/base/techlist/techlist',
+        url_typeid:0,
+        url_name:null,
+        appid:null,
+        path:null,
+        sort:0,
+        state:1,
+        create_time:'2020-04-01 10:44:59'
+      },{
+        id:17,
+        type:2,
+        title:'工长列表',
+        pic:'images/1028/2019/08/wYPdr180ZXpY8x68Yq66dqQ00xP63D.png',
+        clickago_icon:null,
+        clickafter_icon:null,
+        link_type:1,
+        url_type:null,
+        url:'/base/techlist/techlist',
+        url_typeid:0,
+        url_name:null,
+        appid:null,
+        path:null,
+        sort:0,
+        state:1,
+        create_time:'2020-04-01 10:44:59'
+      }],
+      root:'https://zhyframe.fzh.fun/attachment/'
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
